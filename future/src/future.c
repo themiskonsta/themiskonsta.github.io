4d5db8f80527630c13e9c0f56db21d658b83bcad
themiskonsta <themiskonstand@gmail.com>
2025-01-14 13:02:13 +0200

    git push

--- future/src/future.c
+++ future/src/future.c
@@ -2,76 +2,76 @@
 #include <string.h>
 #include <stdlib.h>
 
-double sma(double *array,double y,double x)
+double sma(double *array,double y,double x)     //Function which calculates the simple moving average
 {
     double result=0;
     double sum=0;
-    for(int i=y-x; i<=y; i++)
+    for(int i=y-x; i<=y; i++)       // Loop to calculate the sum of elements from (y - x) to y
     {
-        sum+=array[i];
+        sum+=array[i];      // Put all the values in the sum
     }
-    result=sum/x;
+    result=sum/x;       // Calculate the sma, dividing the sum with the window size
     return result;
 }
 
 int main(int argc, char *argv[])
 {   
-    double x = (argc>3) ? atof(argv[3]): 50;
+    double x = (argc>3) ? atof(argv[3]): 50;    // Read the window size (default:50)
     double *array;
     double count=0;
     int i=0;
     double a;
     
-    if (argc<2)
+    if (argc<2)     //Check if arguments are more than 1 
     {
         printf("Usage: ./future <filename> [--window N (default: 50)]\n");
         return 1;
     }
     
-    FILE *numbers = fopen(argv[1], "r");
+    FILE *numbers = fopen(argv[1], "r");    //Open the file for reading
 
-    if (x==0)
+    if (x<1)    //Check if window size is less than 1
     {
         printf("Window too small!\n");
         return 1;
     }
     
-    if (!numbers) 
+    if (!numbers)       //Check if the file is not opening
     {
         fprintf(stderr, "Error opening file\n");
         return 1;
     }
 
-    while (fscanf(numbers,"%lf", &a)==1)
+    while (fscanf(numbers,"%lf", &a)==1)        //Count the numbers in the file 
     {
         count++;
     }
 
-    rewind(numbers);
-    
-    if (count<x)
+    rewind(numbers);        //Go to the beggining of the file 
+
+    if (count<x)        //Compare the numbers in the file with the window size
     {
         printf("Window too large!\n");
         return 1;
     }
 
-    array = (double*)malloc(count*sizeof(double));
+    array = (double*)malloc(count*sizeof(double));      //Store the numbers from the file in an array
     
-    if (!array) 
+    if (!array)         // Check if memory allocation fails
     {
         printf("Failed to allocate window memory\n");
         fclose(numbers);
         return 1;
     }
 
-    while (fscanf(numbers,"%lf", &array[i])==1)
+    while (fscanf(numbers,"%lf", &array[i])==1)     // Read the numbers from the file and store them in the array
     {
         i++;
     }
-    fclose(numbers);
+    fclose(numbers);        //Close the file
 
-    printf("%lf\n",sma(array,count,x));
+    printf("%lf\n",sma(array,count,x));     //Print the sma for the selected window
 
-    free(array);
+    free(array);        // Free the allocated memory from the array
     return 0;
 }
